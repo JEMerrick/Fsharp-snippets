96bab0bf57d80f9396dcae7c479fd171d29d86a5
JEMerrick <joannaemerrick@gmail.com>
2021-01-30 17:10:43 +0000

    Polygon perimeter complete

--- IO/IO/Program.fs
+++ IO/IO/Program.fs
@@ -2,8 +2,13 @@
 
 open System
 
+//Read many items in one line into a list of ints
+//Needs to be called in a for loop for multiple uses
+
 let InputToList () =
     Console.ReadLine().Split(' ') |> List.ofArray |> List.map (fun v -> int(v))
+
+//Reads all input lines, where one item per line into a list (of ints)
 
 let rec readlines numList = 
     let line = Console.ReadLine()
@@ -11,11 +16,23 @@ let rec readlines numList =
     | null -> []
     | _ -> (line |> int)::(readlines numList)
 
+//Reads all input lines and makes a list where each line = one item in the list 
 let input = 
     stdin.ReadToEnd().Split '\n' 
     |> Array.map(fun x -> int(x)) 
     |> Array.toList
+   
+//Reads all input lines and makes a list of lists
+//Each line = one item in the outer list, each value spaced by ' ' in a line is an item in the inner list
+let rec read numList = 
+    let line = Console.ReadLine()
+    match line with
+    | null -> []
+    | _ -> (line.Split(' ') 
+            |> Array.map (fun v -> int v) 
+            |> Array.toList)::(read numList)
     
+
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
---
+++ PolygonPerimeter/PolygonPerimeter/Program.fs
@@ -0,0 +1,50 @@
+﻿open System
+
+let rec readlines numList = 
+    let line = Console.ReadLine()
+    match line with
+    | null -> []
+    | _ -> (line.Split(' ') 
+            |> Array.map (fun v -> int v) 
+            |> Array.toList)::(readlines numList)
+
+let lengthPoints (points : int list list) =
+    let p1 = points.[0]
+    let p2 = points.[1]
+    let x = float(p1.[0] - p2.[0])
+    let y = float(p1.[1] - p2.[1])
+    sqrt ((x ** 2.0) + (y ** 2.0))
+
+let polygonPerimeter (points : int list list) =
+    //read input
+    //duplicate each point
+    //remove the last value duplicate from the list
+    let pointList = 
+        points
+        |> List.collect (fun v -> List.replicate 2 v) 
+        |> List.rev 
+        |> List.tail 
+        |> List.rev
+    
+    //find the last value and add to start of list
+    let last = List.last pointList
+    let allPoints = List.append [last] pointList
+
+    //chunkby 2 to get each pair of points
+    //find the length between each pair
+    //sum the lengths
+    allPoints 
+    |> List.chunkBySize 2 
+    |> List.map lengthPoints 
+    |> List.reduce (+) 
+    |> printf"%A" 
+
+[<EntryPoint>]
+let main argv =
+    Console.ReadLine() //dont need n
+    
+    
+    readlines [] |> polygonPerimeter
+        
+    
+    0 // return an integer exit code
