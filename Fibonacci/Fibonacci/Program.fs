cb6cb83f68d564e4f83be0833e542607a8a885fd
JEMerrick <joannaemerrick@gmail.com>
2021-02-02 12:07:25 +0000

    fibonacci

---
+++ Fibonacci/Fibonacci/Program.fs
@@ -0,0 +1,14 @@
+﻿open System
+
+let rec fib n =
+    match n with
+    | 0 | 1 -> 0
+    | 2 -> 1
+    | n -> fib(n - 1) + fib(n - 2)
+    
+
+[<EntryPoint>]
+let main argv =
+    let n = Console.ReadLine() |> int
+    fib n |> printf"%A\n" 
+    0 // return an integer exit code
